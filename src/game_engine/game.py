300097da710d318a0fe39bd9cf5651bb33675d95
kev2010 <cloudwiz24@gmail.com>
2024-08-27 23:34:49 -0700

    add start of game

--- src/game_engine/game.py
+++ src/game_engine/game.py
@@ -1,9 +1,10 @@
+import random
 from enum import Enum
 from cards.card_instances import CARD_MAP, SUPPLY_CARD_LIMITS
+from player import PlayerState
 
 DEFAULT_SUPPLY = ["Copper", "Silver", "Gold", "Estate", "Duchy", "Province", "Curse"]
 SIMPLE_SETUP = ["Chapel", "Village", "Smithy", "Moneylender", "Festival", "Laboratory", "Market", "Witch"]
-
 
 class Phase(Enum):
     ACTION = "action"
@@ -11,9 +12,30 @@ class Phase(Enum):
     CLEANUP = "cleanup"
 
 class Game:
-    def __init__(self, kingdom_cards = SIMPLE_SETUP):
+    def __init__(self, kingdom_cards = SIMPLE_SETUP, num_players = 2):
         # Game setup
         self.supply_piles = {CARD_MAP[card]: SUPPLY_CARD_LIMITS[card] for card in DEFAULT_SUPPLY + kingdom_cards}
-        self.players = []
+        self.players = [PlayerState(f"Player {i+1}") for i in range(num_players)]
         self.current_player_turn = 0
         self.current_phase = Phase.ACTION
+    
+    def start_game(self):
+        # Randomly select player to go first
+        self.current_player_turn = random.randint(0, len(self.players) - 1)
+
+        # Initialize each player with 7 coppers, 3 estates and then draw 5 cards
+        for player in self.players:
+            player.discard_pile = [CARD_MAP["Copper"]] * 7 + [CARD_MAP["Estate"]] * 3
+            player.cleanup_cards()
+
+    def buy_card(self, card_name):
+        pass
+
+    def next_phase(self):
+        self.current_phase = Phase((self.current_phase + 1) % len(Phase))
+
+    def next_player(self):
+        self.current_player_turn = (self.current_player_turn + 1) % len(self.players)
+
+    def current_player(self):
+        return self.players[self.current_player_turn]
--- src/game_engine/player.py
+++ src/game_engine/player.py
@@ -1,35 +1,63 @@
+import random
+
 class PlayerState:
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
+
+        # Game state
         self.actions = 0
         self.buys = 0
         self.coins = 0
 
-        self.deck = []
+        # Deck information
+        self.draw_pile = []
         self.hand = []
-        self.play_area = []
-        self.discard = []
-        self.temp_area = []
+        self.played_cards = []
+        self.discard_pile = []
 
     def all_cards(self):
-        cards = []
-        for collection in [self.deck, self.hand, self.play_area, self.discard, self.temp_area]:
-            cards.extend(collection)
-        return cards
+        return self.draw_pile + self.hand + self.played_cards + self.discard_pile
 
     def num_cards(self):
         return len(self.all_cards())
+    
+    def victory_points(self):
+        return sum(card.victory_points for card in self.all_cards())
 
     def play_remaining_treasures(self):
-        remaining_treasures = list(map(lambda card: card.name, filter(lambda card: card.is_treasure(), self.hand)))
-        for card_name in remaining_treasures:
-            self.handle.play(card_name)
+        # Filter out treasure cards from hand and play them
+        for card in filter(lambda card: card.is_treasure(), self.hand):
+            self.handle.play(card.name)
+    
+    def draw_card(self):
+        # Check if draw_pile is empty
+        if not self.draw_pile:
+            # If draw_pile is empty, shuffle discard_pile and set it as draw_pile
+            self.draw_pile = self.discard_pile[:]
+            random.shuffle(self.draw_pile)
+            self.discard_pile.clear()
+        
+        # Draw the top card from draw_pile and add it to hand
+        if self.draw_pile:
+            self.hand.append(self.draw_pile.pop())
+    
+    def cleanup_cards(self):
+        self.actions = 1
+        self.buys = 1
+        self.coins = 0
 
-    def victory_points(self):
-        points = 0
-        for card in self.all_cards():
-            points += card.victory_points.invoke(self.handle, self.game, None)
-        return points
+        # Move all cards from hand to discard pile
+        self.discard_pile.extend(self.hand)
+        self.hand.clear()
+
+        # Move all cards from play area to discard pile
+        self.discard_pile.extend(self.played_cards)
+        self.played_cards.clear()
+
+        # Draw 5 cards
+        for _ in range(5):
+            self.draw_card()
 
     def __str__(self):
-        return 'PlayerState(actions={}, buys={}, coins={}, deck={}, hand={},play_area={}, discard={}, temp_area={}'.format(
-            self.actions, self.buys, self.coins, self.deck, self.hand, self.play_area, self.discard, self.temp_area)
+        return 'PlayerState(actions={}, buys={}, coins={}, draw_pile={}, hand={}, played_cards={}, discard_pile={})'.format(
+            self.actions, self.buys, self.coins, self.draw_pile, self.hand, self.played_cards, self.discard_pile)
